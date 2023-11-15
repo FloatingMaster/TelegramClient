71d19c9596cee704594e0c14135f4c061db8dfaf
Román <roma.webkill@gmail.com>
2023-11-15 20:41:21 +0000

    Initial boilerplate

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TelegramClient
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TelegramClient
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
